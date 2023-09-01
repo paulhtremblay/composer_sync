02ac0c9575a3a3ca3183104c6497ac1605eddcc3
Paul Tremblay <phtremblay@sbgtv.com>
2023-09-01 14:22:24 -0700

    tests complete

--- sync_composer/sync_composer.py
+++ sync_composer/sync_composer.py
@@ -28,7 +28,7 @@ def _get_args():
 
 def _get_ignore_patterns(
         path:Optional[typing.Union[str, os.PathLike]], 
-        verbose:int = 0) -> list:
+        verbosity:int = 0) -> list:
     if path == None:
         return []
     assert isinstance(path, str) #needed for mypy :(
@@ -39,13 +39,14 @@ def _get_ignore_patterns(
         print(ignore)
     return ignore
 
-def ignore_file(path, patterns):
+def ignore_file(path:os.PathLike, patterns:list, verbosity:int = 0):
     for pat in patterns:
         if fnmatch.fnmatch(path, pat):
             return True
     return False
 
-def walk_tree(the_dir:str, ignore:typing.List[str]  
+def walk_tree(the_dir:str, ignore:typing.List[str],
+              verbosity:int = 0
               ) -> typing.Generator:
     for root, dirs, files in os.walk(the_dir):
         for filename in files:
@@ -58,7 +59,8 @@ def list_blobs(
         storage_client:google.cloud.storage.client.Client, 
         bucket_name:str, 
         prefix:str, 
-        delimiter: Optional[str] = None) -> google.api_core.page_iterator.HTTPIterator :
+        delimiter: Optional[str] = None,
+        verbosity:int = 0) -> google.api_core.page_iterator.HTTPIterator :
     
     bucket = storage_client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix = prefix, delimiter=delimiter)
@@ -70,26 +72,36 @@ def get_gcs_hash(blob:google.cloud.storage.blob.Blob)-> str:
 def local_hash(path:str)-> str:
     return  hashlib.md5(open(path,'rb').read()).hexdigest()
 
-def _get_local_d(ignore_patterns:typing.List[str])-> dict:
+def _get_local_d(directory: os.PathLike, 
+                 ignore_patterns:typing.List[str],
+                 verbosity:int = 0)-> dict:
     d = {}
-    for x in walk_tree(the_dir = 'dags', ignore = ignore_patterns):
+    for x in walk_tree(the_dir = directory, ignore = ignore_patterns):
         local_as_hex = local_hash(path = x)
         d[x] = local_as_hex
+    if verbosity > 2:
+        print('local dict is')
+        print(d)
     return d
 
 def _get_storage_d(
         bucket_name:str, 
-        storage_client:google.cloud.storage.client.Client)-> dict:
+        storage_client:google.cloud.storage.client.Client,
+        verbosity:int = 0)-> dict:
     d = {}
     g = list_blobs(storage_client = storage_client, bucket_name = bucket_name, prefix = 'dags')
     for i in g:
         gc_as_hex = get_gcs_hash(blob = i)
         d[i.name] = gc_as_hex
+    if verbosity > 2:
+        print('storage dict is')
+        print(d)
     return d
 
 def get_not_in_local_changed_local(
         storage_dict:dict, local_dict:dict, 
-        ignore_patterns:list)->typing.Tuple[list, list]:
+        ignore_patterns:list,
+        verbosity:int = 0)->typing.Tuple[list, list]:
     not_found_local = []
     diff = []
     for i in storage_dict.keys():
@@ -136,17 +148,28 @@ def upload_files_to_bucket(
             print(f'uploaded "{i}" to "{bucket_name}"')
 
 def sync_bucket(
+        directory:str,
         bucket_name:str, 
         project:str, 
         ignore_path:Optional[os.PathLike] = None, 
         verbosity:int = 0):
-    ignore_patterns = _get_ignore_patterns(path = ignore_path)
+    ignore_patterns = _get_ignore_patterns(
+            path = ignore_path,
+            verbosity = verbosity)
     storage_client = storage.Client(project = project )
-    storage_dict = _get_storage_d(bucket_name = bucket_name, storage_client = storage_client)
-    local_dict = _get_local_d(ignore_patterns = ignore_patterns)
-    not_found_local, diff = get_not_in_local_changed_local(storage_dict = storage_dict,
-                                                           local_dict = local_dict,
-                                                           ignore_patterns = ignore_patterns)
+    storage_dict = _get_storage_d(
+            bucket_name = bucket_name, 
+            storage_client = storage_client,
+            verbosity = verbosity)
+    local_dict = _get_local_d(
+            directory = directory, 
+            ignore_patterns = ignore_patterns,
+            verbosity = verbosity)
+    not_found_local, diff = get_not_in_local_changed_local(
+            storage_dict = storage_dict,
+            local_dict = local_dict,
+            ignore_patterns = ignore_patterns,
+            verbosity = verbosity)
     not_found_storage = get_not_found_storage(storage_dict = storage_dict,
                                               local_dict = local_dict
                                               )
@@ -157,7 +180,7 @@ def sync_bucket(
                            bucket_name = bucket_name,
                            list_of_paths = diff + not_found_storage, 
                            verbosity = verbosity)
-    return not_found_local, diff + not_found_local
+    return not_found_local, diff + not_found_storage
 
 def main():
     args = _get_args()
